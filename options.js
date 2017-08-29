5f70c97e191944a167f2c51f317ed69965b77384
Linda Haviv <Linda.Haviv@foxnews.com>
2017-08-29 10:23:35 -0400

    added more domain options

--- options.js
+++ options.js
@@ -19,7 +19,7 @@ window.DOMAIN_OPTIONS = {
     "default" : {
       "domain" : "testing-foxnews.auth0.com",
       "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
-      "redirectUri": "http://localhost:9000/callback.html",
+      "redirectUri": "https://lindahaviv.github.io/auth0-good-poc/callback.html",
       "responseType": "token id_token",
       "responseMode": "fragment",
       "scope": "openid email nickname"
