fa5c4c5715e59e20afb02f16b7392af57d08f661
Linda Haviv <Linda.Haviv@foxnews.com>
2017-08-29 10:20:53 -0400

    added more domain options

--- options.js
+++ options.js
@@ -14,5 +14,21 @@ window.DOMAIN_OPTIONS = {
     "popup": {
       //"ignoreCasing": true
     }
+  },
+  github: {
+    "default" : {
+      "domain" : "testing-foxnews.auth0.com",
+      "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
+      "redirectUri": "http://localhost:9000/callback.html",
+      "responseType": "token id_token",
+      "responseMode": "fragment",
+      "scope": "openid email nickname"
+    },
+    "silent": {
+      "usePostMessage": true
+    },
+    "popup": {
+      //"ignoreCasing": true
+    }
   }
-}
+};
