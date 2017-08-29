929a8b0feb22201b058174e010189b1507e747cf
Linda Haviv <Linda.Haviv@foxnews.com>
2017-08-29 10:31:10 -0400

    added more domain options

--- options.js
+++ options.js
@@ -1,21 +1,21 @@
 window.DOMAIN_OPTIONS = {
-  localhost: {
-    "default" : {
-      "domain" : "testing-foxnews.auth0.com",
-      "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
-      "redirectUri": "http://localhost:9000/callback.html",
-      "responseType": "token id_token",
-      "responseMode": "fragment",
-      "scope": "openid email nickname"
-    },
-    "silent": {
-      "usePostMessage": true
-    },
-    "popup": {
-      //"ignoreCasing": true
-    }
-  },
-  "lindahaviv.github.io": {
+  // localhost: {
+  //   "default" : {
+  //     "domain" : "testing-foxnews.auth0.com",
+  //     "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
+  //     "redirectUri": "http://localhost:9000/callback.html",
+  //     "responseType": "token id_token",
+  //     "responseMode": "fragment",
+  //     "scope": "openid email nickname"
+  //   },
+  //   "silent": {
+  //     "usePostMessage": true
+  //   },
+  //   "popup": {
+  //     //"ignoreCasing": true
+  //   }
+  // },
+   "lindahaviv.github.io":{
     "default" : {
       "domain" : "testing-foxnews.auth0.com",
       "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
