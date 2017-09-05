a99e30309000e1327f4af6ec32ad77badfc8cc1f
LindaHaviv <linda.haviv@gmail.com>
2017-09-04 21:12:46 -0400

    updated github options

--- options.js
+++ options.js
@@ -49,7 +49,7 @@ window.DOMAIN_OPTIONS = {
       "domain" : "testing-foxnews.auth0.com",
       "clientID": "3ys8tHNcg8hzzUeV-nizksE1lYw3bvBX",
       "audience": "https://sso.foxnews.com/userinfo",
-      "redirectUri": "http://testing-foxnews.auth0.com/auth0-good-poc/callback.html",
+      "redirectUri": "https://lindahaviv.github.io/auth0-good-poc/callback.html",
       "responseType": "token id_token",
       "responseMode": "fragment",
       "scope": "openid email nickname"
